0e5233e09d6c769cc75165286fe9546ea20df64d
Tahereh Gholami <mstgholami@gmail.com>
2021-11-27 23:18:15 +0330

    Implement a throttle function

--- 1.js
+++ 1.js
@@ -1,3 +1,14 @@
 module.exports = function func(fn, ms) {
-
+    let isCalled
+    let lastArgs
+	return (...args) => {
+        lastArgs = args
+		if (!isCalled) {
+			setTimeout(() => {
+				fn(...lastArgs)
+				isCalled = false
+			}, ms)
+			isCalled = true
+		}
+	}
 }
--- tests/1.test.js
+++ tests/1.test.js
@@ -31,4 +31,26 @@ describe("test 1.js", () => {
         expect(fn).toBeCalledTimes(1);
         expect(fn.mock.results[0].value).toBe(3)
     })
+
+    test("it should be called only with the last args", () => {
+		f(1, 2)
+		f(2, 3)
+		f(3, 4)
+		f(4, 5)
+		jest.runAllTimers()
+		expect(fn).toBeCalledTimes(1)
+		expect(fn).toBeCalledWith(4, 5)
+		expect(fn.mock.results[0].value).toBe(9)
+	})
+
+	test("it can be called again after the delay", () => {
+		f(1, 2)
+		setTimeout(() => {
+			f(3, 4)
+		}, 1500)
+		jest.runAllTimers()
+		expect(fn).toBeCalledTimes(2)
+		expect(fn).toHaveBeenNthCalledWith(1, 1, 2)
+		expect(fn).toHaveBeenNthCalledWith(2, 3, 4)
+	})
 })
